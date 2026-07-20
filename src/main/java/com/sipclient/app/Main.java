652a01e66fc26611cf98dbb2f17fcbf226859b4f
A7med3mar4 <ahmed6751241@gmail.com>
2026-07-20 19:12:45 +0300

    Setup JavaFX project and initial application window

---
+++ src/main/java/com/sipclient/app/Main.java
@@ -0,0 +1,30 @@
+package com.sipclient.app;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        Label label = new Label("Welcome to SIP Client");
+
+        StackPane root = new StackPane(label);
+
+        Scene scene = new Scene(root, 600, 400);
+
+        stage.setTitle("SIP Client");
+
+        stage.setScene(scene);
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
